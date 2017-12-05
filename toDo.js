62286038c37a9d7c9a617b5c05978f7b48f4a316
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-05 11:02:44 -0600

    added strike checkbox functionality

--- toDo.js
+++ toDo.js
@@ -1,6 +1,6 @@
 let newToDo = document.querySelector('#toDoInput');
 const addButton = document.querySelector('#addButton');
-
+const lineOut = document.querySelector('.strike');
 const list = document.querySelector('ul');
 
 addButton.addEventListener('click', () => {
@@ -11,9 +11,12 @@ addButton.addEventListener('click', () => {
 
   const p = document.createElement('p');
   p.textContent = newToDo.value;
+  p.style.textDecoration = "none";
 
   const input = document.createElement('input');
   input.type = "checkbox";
+  input.className = "strike";
+
 
   const removeButton = document.createElement('button');
   removeButton.textContent = 'x';
@@ -27,6 +30,20 @@ addButton.addEventListener('click', () => {
   list.appendChild(li);
 
   newToDo.value = '';
+
+});
+
+list.addEventListener('change', (event) => {
+
+  if(event.target.tagName == 'INPUT') {
+    let item = event.target.parentNode.children[0];
+    if(item.style.textDecoration == "none") {
+      item.style.textDecoration = "line-through";
+    } else {
+      item.style.textDecoration = "none";
+    }
+
+  }
 
 });
 
