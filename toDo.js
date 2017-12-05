695496f1b76e4e6f4d55b75a0c20e7f5a73de8a1
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-05 05:15:52 -0600

    added strike checkbox and remove button to each added task

--- toDo.js
+++ toDo.js
@@ -1,17 +1,37 @@
 let newToDo = document.querySelector('#toDoInput');
 const addButton = document.querySelector('#addButton');
-const removeButton = document.querySelector('#removeButton');
+
 const list = document.querySelector('ul');
 
 addButton.addEventListener('click', () => {
   const li = document.createElement('li');
-  li.textContent = newToDo.value;
+
+  const div = document.createElement('div');
+  div.className = "toDoContainer";
+
+  const p = document.createElement('p');
+  p.textContent = newToDo.value;
+
+  const input = document.createElement('input');
+  input.type = "checkbox";
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'x';
+  removeButton.className = "removeButton";
+
+
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(input);
+  div.appendChild(removeButton);
   list.appendChild(li);
+
   newToDo.value = '';
+
 });
 
-removeButton.addEventListener('click', () => {
-  const toDo = document.querySelectorAll('ul li')[0];
-  list.removeChild(toDo);
-  newToDo.value = '';
-});
+// removeButton.addEventListener('click', () => {
+//   const toDo = document.querySelectorAll('ul li')[0];
+//   list.removeChild(toDo);
+//   newToDo.value = '';
+// });
