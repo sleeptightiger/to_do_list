bcb0d786155660ca6f183cc635ebfb44c0cbe2bc
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-05 04:17:47 -0600

    added remove first added button

--- toDo.js
+++ toDo.js
@@ -1,10 +1,17 @@
 let newToDo = document.querySelector('#toDoInput');
 const addButton = document.querySelector('#addButton');
+const removeButton = document.querySelector('#removeButton');
 const list = document.querySelector('ul');
 
 addButton.addEventListener('click', () => {
   const li = document.createElement('li');
   li.textContent = newToDo.value;
   list.appendChild(li);
+  newToDo.value = '';
+});
+
+removeButton.addEventListener('click', () => {
+  const toDo = document.querySelectorAll('ul li')[0];
+  list.removeChild(toDo);
   newToDo.value = '';
 });
